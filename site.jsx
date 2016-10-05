0a502465439674f022b6f9b0d7c49daae96a95c2
Gevorg Harutyunyan <gevharut@gmail.com>
2016-10-04 13:22:14 -1200

    Add page content into responsive container Make tables responsive Update index.html

--- site.jsx
+++ site.jsx
@@ -10,28 +10,30 @@ const React = require('react'),
 class EventsTable extends React.Component {
   render () {
     return (
-      <Table striped bordered condensed hover>
-        <thead>
-          <tr>
-            <th>Monday</th>
-            <th>Tuesday</th>
-            <th>Wednesday</th>
-            <th>Thursday</th>
-            <th>Friday</th>
-            <th>Saturday</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{this.props.schedule.Monday}</td>
-            <td>{this.props.schedule.Tuesday}</td>
-            <td>{this.props.schedule.Wednesday}</td>
-            <td>{this.props.schedule.Thursday}</td>
-            <td>{this.props.schedule.Friday}</td>
-            <td>{this.props.schedule.Saturday}</td>
-          </tr>
-        </tbody>
-      </Table>
+      <div className="table-responsive">
+        <Table striped bordered condensed hover>
+          <thead>
+            <tr>
+              <th>Monday</th>
+              <th>Tuesday</th>
+              <th>Wednesday</th>
+              <th>Thursday</th>
+              <th>Friday</th>
+              <th>Saturday</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{this.props.schedule.Monday}</td>
+              <td>{this.props.schedule.Tuesday}</td>
+              <td>{this.props.schedule.Wednesday}</td>
+              <td>{this.props.schedule.Thursday}</td>
+              <td>{this.props.schedule.Friday}</td>
+              <td>{this.props.schedule.Saturday}</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
     );
   }
 };
@@ -83,93 +85,95 @@ IBM+Innovative+Solutions+and+Technologies+Center/@40.183162,\
           <link rel={"stylesheet"} href={bstrap}/>
         </head>
         <body>
-          <PageHeader style={{paddingLeft: '1rem'}}>
-            iterate hackerspace <small> in Yerevan, Armenia</small>
-          </PageHeader>
-          <Well style={{marginLeft: '1rem', marginRight: '1rem'}}>
-            The hackerspace is a safe place where you can come and
-            use our Debian Linux computers, experiment with open-source code,
-            learn new technologies and make new friends!
-            <br/>
-            <br/>
-            Հաքերսփեյս֊ը հուսալի վայր է, որտեղ կարող եք օգտվել մեր
-            Debian Linux֊ով համակարգիչներից, ձեռք բերել open-source֊ի
-            հետ աշխատելու փորձ, սովորել նոր տեխնոլոգիաներ և ձեռք բերել նոր ընկերներ
-            <br/>
-            <br/>
-            The Hackerspace - безопасное место куда вы можете
-            прийти одни или с друзьями, используя linux Debian
-            машины поэкспериментировать с открытым исходным кодом,
-            изучить новейшие технологии а также завести новых друзей
-          </Well>
-          <Well style={{marginLeft: '1rem', marginRight: '1rem'}}>
-            The hackerspace is open from 9am-9pm, Monday through Saturday, and is
-            located in ISTC (IBM Innovative Solutions and Technologies Center), on
-            the sixth floor of the Linguistics Building at
-            Yerevan State University: <a href={google_link}>Location</a>.
-            Meetups are posted <a href={meetup_link}>here</a> and this is our
-            <a href={fb_link}> Facebook group</a>. Thank you to
-            <a href={istc_link}> ISTC </a> and <a href={eif_link}>EIF</a> for
-            providing us this space and supporting us with the necessary resources.
+          <div className="container">
+            <PageHeader style={{paddingLeft: '1rem'}}>
+              iterate hackerspace <small> in Yerevan, Armenia</small>
+            </PageHeader>
+            <Well style={{marginLeft: '1rem', marginRight: '1rem'}}>
+              The hackerspace is a safe place where you can come and
+              use our Debian Linux computers, experiment with open-source code,
+              learn new technologies and make new friends!
+              <br/>
+              <br/>
+              Հաքերսփեյս֊ը հուսալի վայր է, որտեղ կարող եք օգտվել մեր
+              Debian Linux֊ով համակարգիչներից, ձեռք բերել open-source֊ի
+              հետ աշխատելու փորձ, սովորել նոր տեխնոլոգիաներ և ձեռք բերել նոր ընկերներ
+              <br/>
+              <br/>
+              The Hackerspace - безопасное место куда вы можете
+              прийти одни или с друзьями, используя linux Debian
+              машины поэкспериментировать с открытым исходным кодом,
+              изучить новейшие технологии а также завести новых друзей
+            </Well>
+            <Well style={{marginLeft: '1rem', marginRight: '1rem'}}>
+              The hackerspace is open from 9am-9pm, Monday through Saturday, and is
+              located in ISTC (IBM Innovative Solutions and Technologies Center), on
+              the sixth floor of the Linguistics Building at
+              Yerevan State University: <a href={google_link}>Location</a>.
+              Meetups are posted <a href={meetup_link}>here</a> and this is our
+              <a href={fb_link}> Facebook group</a>. Thank you to
+              <a href={istc_link}> ISTC </a> and <a href={eif_link}>EIF</a> for
+              providing us this space and supporting us with the necessary resources.
 
-            <br/>
-            <br/>
-            Հաքերսփեյս֊ը բաց է երկուշաբթիից շաբաթ՝ 9:00-21:00,
-            ISTC (IBM Innovative Solutions and Technologies Center)
-            ֊ի տարածքում՝ ԵՊՀ լեզվաբանության մասնաշենքի 6֊րդ հարկում։ Սա մեր
-            <a href={fb_link}> ֆեյսբուքյան խումբն</a> է, որտեղ նույնպես
-            տեղադրվում են հանդիպումների մասին տեղեկությունները։
-            Շնորհակալություն <a href={istc_link}> ISTC </a>
-            ֊ին և <a href={eif_link}>EIF</a>-ին
-            Հաքերսփեյսի տարածքը տրամադրելու և անհրաժեշտ ռեսուրսներով աջակցելու համար։
-            <br/>
-            <br/>
-            The Hackerspace  открыт с 9 утра до 9 вечера,
-            с понедельника по субботу, территориалльно находится в
-            ISTC (...) на 6 ом этаже, здания
-            Linguistics около ЕГУ. Наша <a href={fb_link}>facebook группа </a>
-            называется iterate в котором сообщается о каждой новой встрече.
-            Спасибо <a href={istc_link}> ISTC </a> и <a href={eif_link}>EIF </a>
-            за предоставление места, поддержку и всех необходимых ресурсов
-          </Well>
-          <Well style={{marginLeft: '1rem', marginRight: '1rem'}}>
-            Subject to change, we have free workshops starting at 18:30.
-            They usually last until around 20:40. There is no formal registration
-            process, although signup on the specific Meetup date
-            <a href={meetup_link}> here</a> is much appreciated.
-            <br/>
-            <br/>
-            <EventsTable schedule={this.props.schedule_data}/>
-          </Well>
-          <Well style={{marginLeft: '1rem', marginRight: '1rem'}}>
-            Code of Conduct: (adapted from Gothenburg Hackerspace)
-            <br/>
-            <br/>
-            <ul>
-              <li>
-                {coc_notes[0]}
-              </li>
-              <li>
-                {coc_notes[1]}
-              </li>
-              <li>
-                {coc_notes[2]}
-              </li>
-              <li>
-                {coc_notes[3]}
-              </li>
-            </ul>
-          </Well>
-          <Well style={{marginLeft: '1rem', marginRight: '1rem'}}>
-            Special thanks to Sparik Hayrapetyan, Aram Gevorgyan and Robert Adamian
-            for helping with Armenian & Russian translations.
-            <br/>
-            <br/>
-            Built with server-side ReactJS & node; pull requests can be made
-            <a
-              href={'https://github.com/iteratehackerspace/iteratehackerspace.github.io'}>
-             &#160;here</a>
-          </Well>
+              <br/>
+              <br/>
+              Հաքերսփեյս֊ը բաց է երկուշաբթիից շաբաթ՝ 9:00-21:00,
+              ISTC (IBM Innovative Solutions and Technologies Center)
+              ֊ի տարածքում՝ ԵՊՀ լեզվաբանության մասնաշենքի 6֊րդ հարկում։ Սա մեր
+              <a href={fb_link}> ֆեյսբուքյան խումբն</a> է, որտեղ նույնպես
+              տեղադրվում են հանդիպումների մասին տեղեկությունները։
+              Շնորհակալություն <a href={istc_link}> ISTC </a>
+              ֊ին և <a href={eif_link}>EIF</a>-ին
+              Հաքերսփեյսի տարածքը տրամադրելու և անհրաժեշտ ռեսուրսներով աջակցելու համար։
+              <br/>
+              <br/>
+              The Hackerspace  открыт с 9 утра до 9 вечера,
+              с понедельника по субботу, территориалльно находится в
+              ISTC (...) на 6 ом этаже, здания
+              Linguistics около ЕГУ. Наша <a href={fb_link}>facebook группа </a>
+              называется iterate в котором сообщается о каждой новой встрече.
+              Спасибо <a href={istc_link}> ISTC </a> и <a href={eif_link}>EIF </a>
+              за предоставление места, поддержку и всех необходимых ресурсов
+            </Well>
+            <Well style={{marginLeft: '1rem', marginRight: '1rem'}}>
+              Subject to change, we have free workshops starting at 18:30.
+              They usually last until around 20:40. There is no formal registration
+              process, although signup on the specific Meetup date
+              <a href={meetup_link}> here</a> is much appreciated.
+              <br/>
+              <br/>
+              <EventsTable schedule={this.props.schedule_data}/>
+            </Well>
+            <Well style={{marginLeft: '1rem', marginRight: '1rem'}}>
+              Code of Conduct: (adapted from Gothenburg Hackerspace)
+              <br/>
+              <br/>
+              <ul>
+                <li>
+                  {coc_notes[0]}
+                </li>
+                <li>
+                  {coc_notes[1]}
+                </li>
+                <li>
+                  {coc_notes[2]}
+                </li>
+                <li>
+                  {coc_notes[3]}
+                </li>
+              </ul>
+            </Well>
+            <Well style={{marginLeft: '1rem', marginRight: '1rem'}}>
+              Special thanks to Sparik Hayrapetyan, Aram Gevorgyan and Robert Adamian
+              for helping with Armenian & Russian translations.
+              <br/>
+              <br/>
+              Built with server-side ReactJS & node; pull requests can be made
+              <a
+                href={'https://github.com/iteratehackerspace/iteratehackerspace.github.io'}>
+               &#160;here</a>
+            </Well>
+          </div>
         </body>
       </html>
     );
